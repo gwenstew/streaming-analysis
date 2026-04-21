69987626e8ccb83a040f3ba70bab48e8ba4e9f0a
gwenstew <gstew@bu.edu>
2026-04-21 11:40:41 -0400

    function to call tmdb api and fetch trending movies

--- extract/scrapers/tmdb.py
+++ extract/scrapers/tmdb.py
@@ -3,15 +3,34 @@
 # call tmdb once to get genre id mappings
 # could also get top rated movies to see how this changes over time?
 import requests
-import psycopg2
+from dotenv import load_dotenv
 import os
 
+load_dotenv()
+
 API_KEY= os.getenv("TMDB_API_KEY")
+BEARER_TOKEN= os.getenv("TMDB_BEARER_TOKEN")
 
 def get_trending(n:int):
     #fetch top n movies from tmdb
     # get id, title, director, release date, country, genre, popularity,
     
-    url=f""
-    
+    url="https://api.themoviedb.org/3/trending/movie/day?language=en-US"
+
+    headers = {
+        "accept": "application/json",
+        "Authorization": f"Bearer {BEARER_TOKEN}"
+    }
+
+    response = requests.get(url, headers=headers)
+    print(response.status_code)
+    print(response.json())
     return
+
+
+def main():
+    get_trending(5)
+
+
+if __name__ == "__main__":
+    main()
