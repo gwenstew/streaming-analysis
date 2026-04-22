029180933c9e0c7b9600c98654476ef040cececf
gwenstew <gstew@bu.edu>
2026-04-21 21:18:43 -0400

    creates raw schema and two tables: raw.ratings, raw.movies

---
+++ db/init.sql
@@ -0,0 +1,24 @@
+CREATE SCHEMA IF NOT EXISTS raw;
+
+CREATE TABLE IF NOT EXISTS raw.movies (
+    id INT PRIMARY KEY,
+    title TEXT,
+    release_date DATE,
+    language TEXT,
+    genres TEXT[],
+    director TEXT[],
+    cast TEXT[],
+    source TEXT,
+    scraped_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS raw.ratings (
+    id SERIAL PRIMARY KEY,
+    movie_id INT REFERENCES raw.movies(id)
+    title TEXT,
+    score NUMERIC,
+    vote_avg NUMERIC,
+    vote_count NUMERIC,
+    rating_source TEXT,
+    scraped_at TIMESTAMP DEFAULT NOW()
+);
